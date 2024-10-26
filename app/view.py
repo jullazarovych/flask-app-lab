4c64ba110daf34045b9c663a057631a588b8157d
Julia Lazarovych <yuliia.lazarovych.22@pnu.edu.ua>
2024-10-26 23:53:58 +0300

    add app as module

--- app/view.py
+++ app/view.py
@@ -1,7 +1,5 @@
-from flask import Flask, request, redirect, url_for, render_template, abort
-
-app=Flask(__name__)
-app.config.from_pyfile("config.py") #словник
+from flask import  request, redirect, url_for, render_template, abort
+from . import app
 @app.route('/')
 def main():
     return render_template("base.html")
@@ -46,6 +44,3 @@ def deatil_post(id):
 @app.route('/resume')
 def resume():
     return render_template('resume.html', title="Резюме ")
-
-if __name__=='__main__':
-    app.run()
