fced94f68a1210296198904618fb207172e40648
Julia Lazarovych <yuliia.lazarovych.22@pnu.edu.ua>
2024-11-12 13:46:21 +0200

    add wtform with post

--- app/posts/view.py
+++ app/posts/view.py
@@ -1,18 +1,56 @@
-from flask import render_template, abort
+from flask import render_template, abort, flash,redirect, url_for, session
 from . import  post_bp
+from .forms import PostForm
+import json
+import os
 
-posts = [
-    {"id": 1, 'title': 'My First Post', 'content': 'This is the content of my first post.', 'author': 'John Doe'},
-    {"id": 2, 'title': 'Another Day', 'content': 'Today I learned about Flask macros.', 'author': 'Jane Smith'},
-    {"id": 3, 'title': 'Flask and Jinja2', 'content': 'Jinja2 is powerful for templating.', 'author': 'Mike Lee'}
-]
+POSTS_FILE = "posts.json"
+
+def load_posts():
+    if os.path.exists(POSTS_FILE):
+        with open(POSTS_FILE, "r", encoding="utf-8") as file:
+            return json.load(file)
+    return []
+def save_posts(posts):
+    with open(POSTS_FILE, "w", encoding="utf-8") as file:
+        json.dump(posts, file, ensure_ascii=False, indent=4)
+
+@post_bp.route('/add_post', methods=["GET", "POST"])
+def add_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        content = form.content.data
+        is_active = form.is_active.data
+        publish_date = form.publish_date.data.strftime('%Y-%m-%d')
+        category = form.category.data
+        #### 
+        posts = load_posts()
+        author = session.get('username', 'Anonymous')
+        new_post = {
+            "id": len(posts) + 1, 
+            "title": title,
+            "content": content,
+            "author": author,
+            "is_active": is_active,
+            "publish_date": publish_date,
+            "category": category
+        }
+        posts.append(new_post)
+        save_posts(posts)
+        flash(f"Post {title} added succesfully")
+        return redirect(url_for(".get_posts"))
+    
+    return render_template("add_post.html", form=form)
 
 @post_bp.route('/')
 def get_posts():
+    posts = load_posts()
     return render_template("posts.html", posts=posts)
 
 @post_bp.route('/<int:id>')
 def detail_post(id):
+    posts = load_posts()
     if id > 3:
         abort(404)
     post=posts[id-1]
--- app/view.py
+++ app/view.py
@@ -13,3 +13,7 @@ def home():
 @app.route('/resume')
 def resume():
     return render_template('resume.html', title="Резюме ")
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
